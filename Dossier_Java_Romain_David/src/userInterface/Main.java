c7d153ff39120d2361dc660ce695b5a2eaa070cf
Karneidje <etu34192@henallux.be>
2018-04-17 09:31:26 +0200

    ils se mettent l'un en dessous de l'autre
    mais les boutons ne s'affiche qu'au début

--- Dossier_Java_Romain_David/src/userInterface/Main.java
+++ Dossier_Java_Romain_David/src/userInterface/Main.java
@@ -1,9 +1,20 @@
 package userInterface;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main
 {
     public static void main(String[] args)
     {
         MainFrame frame=new MainFrame();
+        try {
+            Connection testeConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPA?useSSL=false", "root", "Sd246aEq");
+        }
+        catch (SQLException SQLE){
+            JOptionPane.showMessageDialog(null,SQLE.getMessage(),"erreur objet connection", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
