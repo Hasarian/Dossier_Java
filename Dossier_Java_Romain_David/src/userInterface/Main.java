c899ba0485344b92d0458490fcb652135a9246ca
hasarian <romain.demoustiez@gmail.com>
2018-04-17 10:08:58 +0200

    librairie bd

--- Dossier_Java_Romain_David/src/userInterface/Main.java
+++ Dossier_Java_Romain_David/src/userInterface/Main.java
@@ -9,9 +9,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        MainFrame frame=new MainFrame();
+        //MainFrame frame=new MainFrame();
         try {
-            Connection testeConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPA?useSSL=false", "root", "Sd246aEq");
+            Connection testeConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPA?useSSL=false", "root", "pwMySQLie2017");
         }
         catch (SQLException SQLE){
             JOptionPane.showMessageDialog(null,SQLE.getMessage(),"erreur objet connection", JOptionPane.ERROR_MESSAGE);
