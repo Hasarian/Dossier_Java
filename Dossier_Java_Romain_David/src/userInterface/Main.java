3f5960b587bd09ddbcee6ee9f8e728773984c490
Hasarian <romain.demoustiez@gmail.com>
2018-04-19 02:32:19 +0200

    trucs qui restaient

--- Dossier_Java_Romain_David/src/userInterface/Main.java
+++ Dossier_Java_Romain_David/src/userInterface/Main.java
@@ -10,11 +10,11 @@ public class Main
     public static void main(String[] args)
     {
         LoginFrame frame=new LoginFrame();
-        try {
+        /*try {
             Connection testeConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPA?useSSL=false", "root", "pwMySQLie2017");
         }
         catch (SQLException SQLE){
             JOptionPane.showMessageDialog(null,SQLE.getMessage(),"erreur objet connection", JOptionPane.ERROR_MESSAGE);
-        }
+        }*/
     }
 }
