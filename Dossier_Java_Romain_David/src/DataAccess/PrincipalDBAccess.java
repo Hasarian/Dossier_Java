54cfdce1260e5878fc0a5cfd17bed437a59eeb99
Karneidje <etu34192@henallux.be>
2018-04-24 12:22:59 +0200

    les boutons commencent à fonctionner

---
+++ Dossier_Java_Romain_David/src/DataAccess/PrincipalDBAccess.java
@@ -0,0 +1,16 @@
+package DataAccess;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PrincipalDBAccess {
+    public PrincipalDBAccess() {
+        try {
+            Connection testeConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SPA?useSSL=false", "root", "pwMySQLie2017");
+        } catch (SQLException SQLE) {
+            JOptionPane.showMessageDialog(null, SQLE.getMessage(), "erreur objet connection", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
