d2d4df0198f121b417132242e79ec8706ad8f4ef
hasarian <romain.demoustiez@gmail.com>
2018-05-08 13:01:29 +0200

    mardi 8/5: aprem

--- Dossier_Java_Romain_David/src/DataAccess/AnimalDBAccess.java
+++ Dossier_Java_Romain_David/src/DataAccess/AnimalDBAccess.java
@@ -1,6 +1,7 @@
 package DataAccess;
 
 import Business.AnimalBusiness;
+import Business.CareGiverBusiness;
 import Business.EspeceBusiness;
 import DataAccess.DAO.DAOAnimal;
 import Model.*;
@@ -55,8 +56,8 @@ public class AnimalDBAccess implements DAOAnimal {
             new BDConnexionError();
         }
     }
-    private void dataToAnimal(ResultSet data)throws ErrorNull, BDConnexionError{
-        AnimalBusiness business=AnimalBusiness.obtenirAnimalBusiness(user);
+    private void dataToAnimal(ResultSet data, CareGiverBusiness userBusiness)throws ErrorNull, BDConnexionError{
+        AnimalBusiness business=AnimalBusiness.obtenirAnimalBusiness(userBusiness.get);
         try {
             EspeceBusiness especeBusiness=EspeceBusiness.obtenirEspeceBusiness();
 
@@ -74,20 +75,22 @@ public class AnimalDBAccess implements DAOAnimal {
             Race race= especeBusiness.obtenirRace(libelleRace,traitDeCaractere,tare,caracteristiqueDuMilieuDeVie,espece);
 
             Integer id = (data.wasNull())?null:new Integer(data.getInt("ficheAnimal.id"));
-
-
+            String remarque=(data.wasNull())?null:data.getString("ficheAnimal.remarque");
+            Integer numCell = (data.wasNull())?null:new Integer(data.getInt("ficheAnimal.numCell"));
+            String nom=(data.wasNull())?null:data.getString("ficheAnimal.nomAnimal");
             GregorianCalendar dateArrive = new GregorianCalendar();
             GregorianCalendar dateDesces = new GregorianCalendar();
-
             dateArrive.setTime(data.getDate("ficheAnimal.dateArrive"));
             dateDesces.setTime(data.getDate("ficheAnimal.dateDesces"));
+            Boolean estDangereux=(data.wasNull())?null:data.getBoolean("ficheAnimal.estDangereux");
             Animal.EtatAnimal [] etatAnimal = Animal.EtatAnimal.values();
             Animal.EtatSoin [] etatSoins = Animal.EtatSoin.values();
-
+            String remarqueSoin=(data.wasNull())?null:data.getString("ficheSoin.remarque");
+            String email=(data.wasNull())?null:data.getString("ficheSoin.email");
             /*Integer id, String remarque, Integer numCell, String nomAnimal, Race race, GregorianCalendar dateArrivee,
                     GregorianCalendar dateDeces, Boolean estDangereux, Animal.EtatAnimal etatAnimal, Animal.EtatSoin etatSoin,
                     String remarqueSoin, Animal.EtatSoin etatFicheSoin, CareGiver careGiver*/
-            business.ajoutAnimal(id);
+            business.ajoutAnimal(id,remarque,numCell,nom,race,dateArrive,dateDesces,estDangereux,etatAnimal,remarqueSoin,etatSoins,);
 
         }
         catch(SQLException e){
