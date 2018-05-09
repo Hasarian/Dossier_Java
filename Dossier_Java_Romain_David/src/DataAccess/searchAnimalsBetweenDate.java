148de3551cb801615a90bf51f33d6e42fe50ed4b
Karneidje <etu34192@henallux.be>
2018-05-09 07:10:20 +0200

    mardi 8/5: aprem

--- Dossier_Java_Romain_David/src/DataAccess/searchAnimalsBetweenDate.java
+++ Dossier_Java_Romain_David/src/DataAccess/searchAnimalsBetweenDate.java
@@ -1,4 +1,7 @@
 package DataAccess;
 
 public class searchAnimalsBetweenDate {
+    select*
+    from ficheAnimal, vaccination, vaccin, fichesoin
+    where ficheAnimal.id = 1 and fichesoin.id = ficheanimal.id and vaccination.id = ficheanimal.id and vaccination.numVaccin = vaccin.numVaccin;
 }
